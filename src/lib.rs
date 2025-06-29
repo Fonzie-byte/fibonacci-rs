a4cd2f125ee40062ea7cd57402b5b83e1abcab54
Fonzie-byte <twistfons@live.nl>
2025-06-29 22:37:03 +0200

    [Feat] Function implemented [Feat] Program is interactive from the CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 /// Returns the nth number from the Fibonacci sequence.
-/// 
+///
 /// # Examples
 /// ```
-/// use fibonacci::fibonacci;
-/// 
-/// assert_eq!(fibonacci(0), 0);
-/// assert_eq!(fibonacci(1), 1);
-/// assert_eq!(fibonacci(2), 1);
-/// assert_eq!(fibonacci(3), 2);
-/// assert_eq!(fibonacci(4), 3);
-/// assert_eq!(fibonacci(5), 5);
-/// assert_eq!(fibonacci(12), 144);
-/// assert_eq!(fibonacci(1000), 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875);
+/// use fibonacci::recursive_fibonacci;
+///
+/// assert_eq!(recursive_fibonacci(0), 0);
+/// assert_eq!(recursive_fibonacci(1), 1);
+/// assert_eq!(recursive_fibonacci(2), 1);
+/// assert_eq!(recursive_fibonacci(3), 2);
+/// assert_eq!(recursive_fibonacci(4), 3);
+/// assert_eq!(recursive_fibonacci(5), 5);
+/// assert_eq!(recursive_fibonacci(12), 144);
 /// ```
-pub fn fibonacci(n: u32) -> u32 {
-    todo!("Implement")
+pub fn recursive_fibonacci(n: u32) -> usize {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use fibonacci::recursive_fibonacci;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let n = env::args()
+        .nth(1)
+        .expect("Please provide an argument.")
+        .parse::<u32>()
+        .expect("The first argument must be a positive integer.");
+    let fibonacci = recursive_fibonacci(n);
+
+    println!("The {}th Fibonacci number is {}", n, fibonacci);
 }
