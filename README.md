411761b87c73b63a98f0027ca2819ab628389798
Fonzie-byte <twistfons@live.nl>
2025-06-29 22:23:48 +0200

    [Feat] Tests before anything else /proud [Feat] Rust GitHub Actions [Feat] Updated README.md with link [Feat] Updated .gitignore with IDE stuff to keep my repo pure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # fibonacci-rs
- An example assignment from the Rust Book at the end of chapter 3.5 
+ An example assignment from the [Rust Book at the end of chapter 3.5](https://rust-book.cs.brown.edu/ch03-05-control-flow.html). 
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+/// Returns the nth number from the Fibonacci sequence.
+/// 
+/// # Examples
+/// ```
+/// use fibonacci::fibonacci;
+/// 
+/// assert_eq!(fibonacci(0), 0);
+/// assert_eq!(fibonacci(1), 1);
+/// assert_eq!(fibonacci(2), 1);
+/// assert_eq!(fibonacci(3), 2);
+/// assert_eq!(fibonacci(4), 3);
+/// assert_eq!(fibonacci(5), 5);
+/// assert_eq!(fibonacci(12), 144);
+/// assert_eq!(fibonacci(1000), 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875);
+/// ```
+pub fn fibonacci(n: u32) -> u32 {
+    todo!("Implement")
+}
